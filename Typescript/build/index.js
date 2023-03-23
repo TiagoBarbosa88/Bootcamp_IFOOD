17ae57e8d4590df4e269b2511601f6afb81c18dc
TiagoBarbosa88 <sombr43@gmail.com>
2023-03-22 21:53:36 -0300

    funções em Typescript

--- Typescript/build/index.js
+++ Typescript/build/index.js
@@ -7,3 +7,12 @@ function addNumber(x, y) {
     return x + y;
 }
 let soma = addNumber(4, 8);
+console.log(soma);
+function addHello(name) {
+    return `Hello ${name}`;
+}
+console.log(addHello('Tiago'));
+function callToPhone(phone) {
+    return phone;
+}
+console.log(callToPhone(1211212121));
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -8,3 +8,23 @@ function addNumber(x: number, y: number): number {
 }
 
 let soma: number = addNumber(4,8)
+console.log(soma);
+
+
+function addHello(name: string): string {
+  return `Hello ${name}` 
+}
+
+console.log(addHello('Tiago'));
+
+function callToPhone( phone: number | string): number | string {
+  return phone;
+
+}
+
+console.log(callToPhone(1211212121));
+
+
+async function getDatabase(id: number | string): Promise<string | number>{
+  return "Tiago"
+}
